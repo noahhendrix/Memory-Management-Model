79478b0e6315a53f01b02fcd7f195306ab239987
Noah Hendrix <noahhendrix@gmail.com>
2011-02-17 22:05:45 -0600

    implement job manager class

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 main:main.o
 	g++ -g main.o -o main
 
-main.o:main.cpp
+main.o:main.cpp lib/List.h
 	g++ -c -g main.cpp
 
 clean:
--- main.cpp
+++ main.cpp
@@ -1,11 +1,56 @@
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
+#include <fstream>
+
+#include "lib/List.h"
 
 using namespace std;
 
+class JobManager {
+private:
+  struct Job {
+    int process_id,
+      memory_required,
+      time_required;
+  };
+  
+  List<Job*> job_list;
+public:
+  void add(int process_id, int memory_required, int time_required) {
+    //initialize new job and fill it's values
+    Job *newJob = new Job;
+    newJob->process_id = process_id;
+    newJob->memory_required = memory_required;
+    newJob->time_required = time_required;
+    
+    //insert into job list
+    job_list.push(newJob);
+  }
+  
+  void allocate_memory()
+  {
+    
+  }
+  
+  void print() {
+    job_list.print();
+  }
+};
+
 int main()
 {
-  cout << "Hello World";
+  JobManager available_jobs;
+  
+  ifstream dataFile("test/processes.txt");
+  int process_id, memory_required, time_required;
+  
+  if (dataFile.is_open()) {
+    while (dataFile >> process_id >> memory_required >> time_required)
+      available_jobs.add(process_id, memory_required, time_required);
+  }
+  
+  available_jobs.print();
+  
+  dataFile.close();
+  
   return 0;
 }
